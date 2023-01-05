163091910fa28fdeb2e3d848f244d2c14fa6ff59
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-01-05 17:26:20 +0500

    Implement form submit function to send data to API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './index.css';
+import { setData } from './modules/restApi.js';
+
+const form = document.querySelector('form');
+const userName = document.querySelector('#name');
+const userScore = document.querySelector('#score');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const score = {
+    user: userName.value,
+    score: userScore.value,
+  };
+  userName.value = '';
+  userScore.value = '';
+  await setData(score);
+});
