a0f5e76bbdc6d4b29080b022145450dad61e21e0
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-03-02 07:05:55 +0500

    Add logic for popup notification

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const userName = document.querySelector('#name');
 const userScore = document.querySelector('#score');
 const scoresList = document.querySelector('#scores-list');
 const refreshBtn = document.querySelector('#refresh-btn');
+const notification = document.querySelector('#notification');
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
@@ -16,12 +17,20 @@ form.addEventListener('submit', async (event) => {
   userName.value = '';
   userScore.value = '';
   await setData(score);
+  // Show the notification after the data is submitted
+  notification.classList.add('show');
+  // Hide the notification after a short delay
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 3000);
 });
 
 // Get List of Scores on Page Load
 const getScores = async () => {
   scoresList.innerHTML = '';
   const scores = await getData();
+  // Sort Scores in Descending Order
+  scores.result.sort((a, b) => b.score - a.score);
   scores.result.forEach((item) => {
     const li = document.createElement('li');
     li.classList.add('score');
